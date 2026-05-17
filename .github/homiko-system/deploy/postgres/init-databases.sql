6bcb8372415d37ad42067374703a702486af4912
ikbal.kazanci <ikbal.kazanci@trendyol.com>
2026-05-18 01:22:52 +0300

    Revert "user crud"
    This reverts commit 726f277e958cdbd3cc56ef2d1922701a4f6c8226.

--- .github/homiko-system/deploy/postgres/init-databases.sql
+++
@@ -1,3 +0,0 @@
--- Runs once on empty data volume. Creates the two app databases on the shared instance.
-CREATE DATABASE "homiko-stage";
-CREATE DATABASE "homiko-prod";
--- .github/homiko-system/merge-into-repo-root/src/main/java/com/homiko/user/AppUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.homiko.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-
-    boolean existsByEmailIgnoreCase(String email);
-}
